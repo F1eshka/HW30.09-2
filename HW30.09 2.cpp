5843d2d03d067753e2d682c1ab6b00e1bd9dd47f
F1eshka <144143888+F1eshka@users.noreply.github.com>
2023-09-24 21:17:00 +0300

    Add files via upload

---
+++ HW30.09 2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	int count = 20;
+	int chtoto = 12;
+	string elem = "@";
+	bool lie = true;
+
+	SetConsoleTextAttribute(h, chtoto);
+	for (int i = 0; i < cout; i++) {
+		cout << elem;
+		if (lie) cout << "\n";
+	}
+}
